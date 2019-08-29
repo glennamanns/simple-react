6dd02bdc782cd2e0d5fdeac7e21a1e8497985121
Glenna Manns <glennamanns@gmail.com>
2019-08-29 10:47:22 -0700

    this is for a PR

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, today is Thursday and this is a demo
+          Hello, today is Thursday and this is a test BRANCHHHHH
         </p>
         <a
           className="App-link"
